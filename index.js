e0d41a4108db2429bcdc39648a2b5c15d815448c
Jatupon Jantri <jameball007@gmail.com>
2018-01-24 01:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal(animal) {
+function yourAnimal() {
   
   // How can we make sure that this function
   // and the above function both pass?
